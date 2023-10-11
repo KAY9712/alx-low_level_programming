33ec668fbfe5dbe423747df05cb9b87159c73d2f
KAY9712 <mahlatsekoketso4@gmail.com>
2023-10-11 12:50:32 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,13 +24,9 @@ exit(98);
 }
 
 
-
-
 num1 = atoi(argv[1]);
 op = argv[2];
 num2 = atoi(argv[3]);
-
-
 
 
 if (get_op_func(op) == NULL || op[1] != '\0')
@@ -38,8 +34,6 @@ if (get_op_func(op) == NULL || op[1] != '\0')
 printf("Error\n");
 exit(99);
 }
-
-
 
 
 if ((*op == '/' && num2 == 0) |
@@ -50,11 +44,7 @@ exit(100);
 }
 
 
-
-
 printf("%d\n", get_op_func(op)(num1, num2));
-
-
 
 
 return (0);
